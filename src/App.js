cdefc614d393335b19e869df413114179e969e95
farpk009 <farpk009@gmail.com>
2019-07-13 12:55:20 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+         Assalaam u alaikum from MMA. We learning to create a web application my first web site!!!
+		 </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
